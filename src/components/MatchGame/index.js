500432e91ee8c40bbc573bfedc94f41d2ebdcd67
Abhinay Ponaganti <ponagantiabhinay@gmail.com>
2024-02-24 16:16:26 +0000

    Match Game App

---
+++ src/components/MatchGame/index.js
@@ -0,0 +1,13 @@
+import {Component} from 'react'
+import GameStructure from '../GameStructure'
+
+class MatchGame extends Component {
+  render() {
+    return (
+      <div>
+        <GameStructure />
+      </div>
+    )
+  }
+}
+export default MatchGame
